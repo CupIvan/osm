9392c19d4770262a0f9e1a16e61e8aa3a559dd44
CupIvan <mail@cupivan.ru>
2018-06-09 11:11:33 +0300

    обновил движок Leaflet

--- i/map.js
+++ i/map.js
@@ -1,3 +1,13 @@
+if (!window.L)
+{
+	document.write(''
+		+'<l'+'ink rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"'
+			+'integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin=""/>'
+		+'<s'+'cript src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"'
+			+'integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></scrip'+'t>'
+	)
+}
+
 var map = {
 init: function(params){
 	params = params || {};
