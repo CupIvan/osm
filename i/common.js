56fe16d833c0b0f2c0acfbdf453f316dfcdfa4fa
CupIvan <mail@cupivan.ru>
2019-03-21 10:20:51 +0300

    растяжка GitHub ribbon

---
+++ i/common.js
@@ -0,0 +1,6 @@
+$(function(){
+	var div = $('div')
+	div.innerHTML = '<a href="https://github.com/CupIvan/osm" title="Открыть репозиторий на GitHub" target="_blank"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"'
+		+'style="position: fixed; right: 0; top: 0; z-index: 9999"></a>'
+	document.body.appendChild(div)
+})
