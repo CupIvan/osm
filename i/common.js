8a86abb24758a4829fc4a03aad3d63d04e45d73a
bopoh13 <bopoh13@ya.ru>
2020-02-20 16:42:03 +0300

    Добавлена подложка Wikimapia, исправлены ссылки Не читал документацию, как перенести кнопку подложки на 'map1'

--- i/common.js
+++ i/common.js
@@ -1,6 +1,6 @@
 $(function(){
 	var div = $('div')
 	div.innerHTML = '<a href="https://github.com/CupIvan/osm" title="Открыть репозиторий на GitHub" target="_blank"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"'
-		+'style="position: fixed; right: 0; top: 0; z-index: 9999"></a>'
+		+'style="width: 119px; height: 119px; position: fixed; right: 0; top: 0; z-index: 99"></a>'
 	document.body.appendChild(div)
 })
