4952a76cb8d607dce76151f33ab4a775e92d9fd8
CupIvan <mail@cupivan.ru>
2020-04-26 12:37:29 +0300

    overpass - рефакторинг логирования

--- overpass/index.php
+++ overpass/index.php
@@ -3,14 +3,14 @@
 if (strpos(@$_SERVER['HTTP_ORIGIN'], 'cupivan.ru'))
 	header('Access-Control-Allow-Origin: *');
 
-define('DIR', '/tmp/cache/');
+define('DIR', '/tmp/cache_osm/');
 if (!file_exists(DIR)) mkdir(DIR, 0777, true);
 
 $data = @$_GET['data'];
 
 $fname = DIR.md5($data).'.json';
 
-$log = date('H:i:s d.m.Y')." - ".$_SERVER['REMOTE_ADDR'];
+$log = date('H:i:s d.m.Y')."\t".$_SERVER['REMOTE_ADDR']."\t";
 
 // очистка старых кешей каждые ~100 запросов
 if (!mt_rand(0, 100)) { exec('find '.DIR.' -mtime +7 -delete'); $log .= " CLEAR"; }
@@ -18,7 +18,7 @@ if (!mt_rand(0, 100)) { exec('find '.DIR.' -mtime +7 -delete'); $log .= " CLEAR"
 if (file_exists($fname) && filemtime($fname) > time() - 2*3600)
 {
 	$page = file_get_contents($fname);
-	$log .= ' CACHE';
+	$log .= 'CACHE';
 }
 
 if (empty($page))
@@ -35,12 +35,13 @@ if (empty($page))
 		@file_put_contents($fname, '{}');
 	$server = $servers[mt_rand(0, count($servers)-1)];
 	$url  = "$server/?data=".urlencode($data);
+
 	$page = @file_get_contents($url);
-	if ($page) @file_put_contents($fname, $page); else { unlink($fname); $page = '{"state": "error", "url": "'.$url.'"}'; }
-	$log .= ' DOWNLOAD';
+	if ($page) @file_put_contents($fname, $page); else { @unlink($fname); $page = '{"state": "error", "url": "'.$url.'"}'; }
+	$log .= 'DOWNLOAD';
 }
 
-$log .= " - $fname - $data\n";
+$log .= round(strlen($page)/1024)."Kb\t$fname\t$data\n";
 
 @file_put_contents(DIR.date('Y-m-d').'.log', $log, FILE_APPEND);
 
