6da2436d2cd59490ab2e9d9682ad1f7a44aa0d07
CupIvan <mail@cupivan.ru>
2020-06-01 16:12:04 +0300

    outpost - добавил Онлайн Трейд

--- outpost/points.php
+++ outpost/points.php
@@ -42,11 +42,31 @@ switch (@$_REQUEST[$x='brand'])
 			return $res;
 		});
 		break;
+	case 'Onlinetrade':
+		echo get_points($_REQUEST[$x], function(){
+			$st = get_page('https://www.onlinetrade.ru/shops/');
+			$n1 = strpos($st, 'var firstUse');
+			$n2 = strpos($st, 'YMapsID_shoplist');
+			$st = substr($st, $n1, $n2-$n1);
+			if (preg_match_all('#Lon.+?(\d[\d.]+).+?Lat.+?(\d[\d.]+)#', $st, $m, PREG_SET_ORDER))
+			foreach ($m as $a)
+			{
+				$res[] = [
+					'lat' => $a[2],
+					'lon' => $a[1],
+				];
+			}
+
+			return $res;
+		});
+		break;
 }
 
 function get_points($brand, $parser)
 {
-	$cache = './cache/'.$_REQUEST['brand'].'.json';
+	$cache = '/tmp/cache_osm/'.$_REQUEST['brand'].'.json';
+	$dir = dirname($cache);
+	if (!file_exists($dir)) mkdir($dir, 0777, true);
 	if (time() - @filemtime($cache) < 7*24*3600) return file_get_contents($cache);
 
 	$res = $parser();
@@ -59,7 +79,7 @@ function get_points($brand, $parser)
 
 function get_page($url)
 {
-	$cache = './cache/'.md5($url).'.html';
+	$cache = '/tmp/cache_osm/'.md5($url).'.html';
 	if (time() - @filemtime($cache) < 24*3600) return file_get_contents($cache);
 	$st = file_get_contents($url);
 	if ($st) file_put_contents($cache, $st);
